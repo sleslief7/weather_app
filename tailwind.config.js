c7e4dfdfb981040b9ef8ad69fbdd69a2ccec5151
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-07-11 18:35:44 -0400

    Setup project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/index.html', './src/**/*.js'],
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
